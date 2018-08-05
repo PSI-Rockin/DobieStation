8454b0d117877e4254859149ea96ad28e0081ae2
refractionpcsx2 <refraction@gmail.com>
2018-08-05 19:39:57 +0100

    BIOS fix

--- src/core/ee/emotion_vu0.cpp
+++ src/core/ee/emotion_vu0.cpp
@@ -8,8 +8,7 @@ void EmotionInterpreter::cop2_special(VectorUnit &vu0, uint32_t instruction)
       * We're flushing pipelines for VU0, as accurately handling COP2's pipelining is painful.
       * I don't yet have a good solution for this that doesn't murder performance.
       */
-    vu0.update_mac_pipeline();
-    vu0.update_div_pipeline();
+    vu0.flush_pipes();
     uint8_t op = instruction & 0x3F;
 
     switch (op)
