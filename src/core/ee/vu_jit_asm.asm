dc57fe618ebd350c7bd429cb3c820525e9ce8ec7
refractionpcsx2 <refraction@gmail.com>
2019-02-15 01:05:21 +0000

    Fix up the asm file

--- src/core/ee/vu_jit_asm.asm
+++ src/core/ee/vu_jit_asm.asm
@@ -6,36 +6,36 @@ EXTERN exec_block:PROC
 .code
 
 run_vu_jit PROC
-	push rbp
-	;mov rbp, rsp
-	push rsi
-	push rbx
-	push rcx
-	push rdx
+    push rbp
+    mov rbp, rsp
+    push rsi
+    push rbx
+    push rcx
+    push rdx
     push r12
     push r13
     push r14
     push r15
     push rdi
 
-	sub rsp, 32
+    sub rsp, 32
     call exec_block
-	add rsp, 32
-	sub rsp, 32
+    add rsp, 32
+    sub rsp, 32
     call rax
-	add rsp, 32
+    add rsp, 32
 
     pop rdi
     pop r15
     pop r14
     pop r13
     pop r12
-	pop rdx
-	pop rcx
+    pop rdx
+    pop rcx
     pop rbx
-	pop rsi
-	pop rbp
-	ret
+    pop rsi
+    pop rbp
+    ret
 run_vu_jit ENDP
 
 END
