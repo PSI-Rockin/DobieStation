ce1f27cd185c5f36e74993b579bb0c2375c0abc0
PSI-Rockin <PSI-Rockin@users.noreply.github.com>
2019-02-28 21:00:21 -0500

    Added PSMCT32Z texture format
    Also clamp initial S, T, and Q values

--- src/core/gif.cpp
+++ src/core/gif.cpp
@@ -89,6 +89,15 @@ void GraphicsInterface::process_PACKED(uint128_t data)
             uint32_t s = data1 & 0xFFFFFF00;
             uint32_t t = (data1 >> 32) & 0xFFFFFF00;
             uint32_t q = data2 & 0xFFFFFF00;
+
+            if ((s & 0x7F800000) == 0x7F800000)
+                s = (s & 0x80000000) | 0x7F7FFFFF;
+
+            if ((t & 0x7F800000) == 0x7F800000)
+                t = (t & 0x80000000) | 0x7F7FFFFF;
+
+            if ((q & 0x7F800000) == 0x7F800000)
+                q = (q & 0x80000000) | 0x7F7FFFFF;
             internal_Q = *(float*)&q;
             gs->set_ST(s, t);
         }
