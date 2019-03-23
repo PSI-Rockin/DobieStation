fb7d24c4e965b5ce3e441d1ee98876c776a9b06c
RedPanda4552 <redpanda4552@gmail.com>
2019-03-23 18:28:51 -0400

    Incomplete memcard implementation
    Totally not rebased

--- src/core/iop/memcard.cpp
+++ src/core/iop/memcard.cpp
@@ -1,6 +1,308 @@
 #include "memcard.hpp"
+#include <cstdint>
+#include <string>
+#include <array>
+#include <vector>
+#include <fstream>
+#include <queue>
 
 Memcard::Memcard()
 {
+	
+}
 
+bool Memcard::is_formatted()
+{
+	return formatted;
+}
+
+bool Memcard::address_in_bounds(uint32_t address, uint32_t length)
+{
+	return address + length <= oem_memcard_bytes;
+}
+
+// Create a file on disk with all bits initialized to their default state, 1.
+// The file is unusable in this state; it must be formatted by a game or BIOS.
+bool Memcard::new_memcard_file(std::string file_name)
+{
+	std::ofstream file(file_name);
+
+	if (!file.is_open())
+	{
+		printf("[MCD] (WARN) Attempted to open ofstream on empty file %s, but it either cannot be opened or could not be created! Aborting...", file_name);
+		return false;
+	}
+
+	// Write 0xff to entire card
+	for (int i = 0; i < oem_memcard_bytes; i++) {
+		file.put(0xff);
+	}
+
+	file.close();
+
+	return true;
+}
+
+// Open the memcard file_name, mounting its contents to this memcard.
+bool Memcard::open(std::string file_name)
+{
+	std::ifstream file(file_name);
+
+	if (!file.is_open())
+	{
+		printf("[MCD] (WARN) Attempted to open ifstream on memcard file %s, but it either cannot be opened or does not exist! Aborting...", file_name);
+		return false;
+	}	
+
+	file.read(raw_data.data(), oem_memcard_bytes);
+
+	if (file)
+		printf("[MCD] (INFO) Successfully read all bytes into memory!");
+	else
+		printf("[MCD] (WARN) Failed to read all bytes into memory! Only read %d!", file.gcount());
+
+	file.close();
+	this->file_name = file_name;
+	this->formatted = true;
+
+	for (int i = 0; i < format_string.length(); i++) {
+		if (raw_data.at(i) != format_string.at(i)) {
+			this->formatted = false;
+			break;
+		}
+	}
+
+	// Check if backup block 2 (labelled 2 in documentation, but actually the
+	// first on the card physically) is not erased. Page 16352 is the first page
+	// of backup block 2.
+	if (!is_erase_block_erased(16352 * bytes_per_page))
+	{
+		printf("[MCD] (WARN) Backup block 2 was NOT erased when this memcard was opened! The last write op on this memcard may have failed! The emulated PS2 should be restoring the old data soon, if it hasn't already...");
+	}
+
+	return true;
+}
+
+// Read length bytes from memcard starting at address into destination.
+bool Memcard::read(std::vector<uint8_t> destination, uint32_t address, uint32_t length)
+{
+	if (!address_in_bounds(address, length))
+	{
+		printf("[MCD] (WARN) Attempted to read out-of-bounds memcard address %d! Aborting...", address);
+		return false;
+	}
+
+	memcpy(destination.data(), &raw_data[address], length);
+	return true;
+}
+
+// Write length bytes from source starting at address into memcard.
+bool Memcard::write(std::vector<uint8_t> source, uint32_t address)
+{
+	if (!address_in_bounds(address, source.size()))
+	{
+		printf("[MCD] (WARN) Attempted to write out-of-bounds memcard address %d! Aborting...", address);
+		return false;
+	}
+
+	// Check if a bitwise and of source and address bytes would give a result
+	// different from the source byte. If so, that means something else
+	// currently exists here, or the erase block containing address has not been
+	// wiped yet. 
+	for (int i = 0; i < source.size(); i++)
+	{
+		if (raw_data[address + i] & source[i] != source[i])
+		{
+			printf("[MCD] (WARN) Attempted to write to memcard address %d, but its erase block hasn't been erased yet! Aborting...", address + i);
+			return false;
+		}
+	}
+
+	// Info printf's for when an important sector is written
+	if (address < 528) // Superblock
+	{
+		printf("[MCD] (INFO) Memcard write op on superblock (addr = %d, size = %d)", address, source.size());
+	}
+	else if (address < checksum_address + 8) // Checksum
+	{
+		printf("[MCD] (INFO) Memcard write op on checksum (addr = %d, size = %d)", address, source.size());
+	}
+	else if (address < 8448) // Unused space
+	{
+		printf("[MCD] (INFO) Memcard write op on unused card space (addr = %d, size = %d)", address, source.size());
+	}
+	else if (address < 9504) // Indirect FAT
+	{
+		printf("MCD] (INFO) Memcard write op on indirect FAT (addr = %d, size = %d)", address, source.size());
+	}
+	else if (address < 43296) // FAT
+	{
+		printf("MCD] (INFO) Memcard write op on FAT (addr = %d, size = %d)", address, source.size());
+	}
+
+	std::ofstream file(this->file_name);
+
+	if (!file.is_open())
+	{
+		printf("[MCD] (WARN) Attempted to open ofstream on %s, but it cannot be opened! Aborting...", this->file_name);
+		return false;
+	}
+
+	memcpy(&raw_data[address], source.data(), source.size());
+	file.seekp(address);
+	file.write((char*) source.data(), source.size());
+	file.close();
+}
+
+// Get the start of the erase block that contains address.
+uint32_t Memcard::get_erase_block_start(uint32_t address)
+{
+	return ((address / bytes_per_page) / erase_block_pages) * erase_block_pages * bytes_per_page;
+}
+
+// Erase the erase block that contains address
+bool Memcard::erase_block(uint32_t address)
+{
+	std::vector<uint8_t> blank;
+	blank.assign(erase_block_pages * bytes_per_page, 0xff);
+	write(blank, get_erase_block_start(address));
+}
+
+// Test if the erase block containing address has been erased.
+bool Memcard::is_erase_block_erased(uint32_t address)
+{
+	uint32_t start_address = get_erase_block_start(address);
+
+	for (int i = 0; i < erase_block_pages * bytes_per_page; i++)
+	{
+		if (raw_data[start_address + i] != 0xff)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void Memcard::op_start(std::queue<uint8_t>& FIFO, uint8_t value)
+{
+	switch (mode)
+	{
+	case PAGE:				op_page(FIFO, value);			break;
+	case PAGE_ERASE:		op_page_erase(FIFO, value);		break;
+	case PAGE_WRITE:		op_page_write(FIFO, value);		break;
+	case PAGE_READ:			op_page_read(FIFO, value);		break;
+	case GET_TERMINATOR:	op_get_terminator(FIFO, value);	break;
+	case AUTHENTICATION:	op_authentication(FIFO, value);	break;
+	case PS1:				op_ps1(FIFO, value);			break;
+	default:				printf("[MCD] (WARN) WTF");		break;
+	}
+}
+
+void Memcard::op_page(std::queue<uint8_t>& FIFO, uint8_t value)
+{
+	uint8_t xor = 0;
+
+	switch (value)
+	{		// Value == ...
+	case 2: // First byte of target address
+		page_index = value;
+		xor = value;
+		break;
+	case 3: // Second
+		page_index = value << 8;
+		xor ^= value;
+		break;
+	case 4: // Third
+		page_index = value << 16;
+		xor ^= value;
+		break;
+	case 5: // Fourth
+		page_index = value << 24;
+		xor ^= value;
+		break;
+	case 6: // XOR of bytes 1-4
+		good_page = value == xor;
+		break;
+	// Nothing to do for 7?
+	case 8:
+		page_address = page_index * bytes_per_page;
+		break;
+	case 9:
+		std::queue<uint8_t> empty;
+		FIFO.swap(empty);
+		switch (command)
+		{
+		case 0x21:
+			break;
+		case 0x22:
+			break;
+		case 0x23:
+			break;
+		}
+	}
+}
+
+void Memcard::op_page_erase(std::queue<uint8_t>& FIFO, uint8_t value)
+{
+	std::queue<uint8_t> empty;
+
+	// PCSX2 seems to be using case 0 and default to control back to back erase
+	// calls. That is, each operation starts with 0x81 and is followed by other
+	// bytes. Then, the next 0x81 signals the start of the next. Lets try a less
+	// hacked up control here.
+	switch (FIFO.size())
+	{
+	case 0:
+		if (value != 0x81)
+		{
+			printf("[MCD] (INFO) Expected 0x81, got %d, flushing FIFO and waiting...", value);
+			FIFO.swap(empty);
+		}
+		else
+		{
+			printf("[MCD] (INFO) Got 0x81, completing the rest of erase command...");
+		}
+			
+		break;
+	case 1:
+
+		break;
+	default:
+
+		break;
+	}
+}
+
+void Memcard::op_page_write(std::queue<uint8_t>& FIFO, uint8_t value)
+{
+
+}
+
+void Memcard::op_page_read(std::queue<uint8_t>& FIFO, uint8_t value)
+{
+
+}
+
+void Memcard::op_get_terminator(std::queue<uint8_t>& FIFO, uint8_t value)
+{
+
+}
+
+void Memcard::op_authentication(std::queue<uint8_t>& FIFO, uint8_t value)
+{
+
+}
+
+void Memcard::op_ps1(std::queue<uint8_t>& FIFO, uint8_t value)
+{
+	printf("[MCD] (INFO) PS1 memcard op attempted, but not implemented yet.");
+}
+
+// Push constant 0x2b, then whatever the memcard has as it's terminator. Seems
+// to be used to finish a command sequence.
+void Memcard::push_terminators(std::queue<uint8_t>& FIFO)
+{
+	FIFO.push(0x2b);
+	FIFO.push(terminator);
 }
--- src/core/iop/memcard.hpp
+++ src/core/iop/memcard.hpp
@@ -1,11 +1,98 @@
 #ifndef MEMCARD_HPP
 #define MEMCARD_HPP
 #include <cstdint>
+#include <string>
+#include <array>
+#include <vector>
+#include <queue>
+
+// Memcard constants and other information largely retrieved from Ross Ridge's
+// writeup: https://www.ps2savetools.com/ps2memcardformat.html
+
+// A note about the port/slot relationship:
+// Ports directly map to the physical ports on the base console.
+// Slots map to the ports. Without multitaps, port 0 is also slot 0, port 1 is
+// also slot 1. With multitaps, port 0 on the console now has slot 0, PLUS 2, 3,
+// and 4. Port 1 then has slot 1, PLUS 5, 6, and 7.
+
+// A note about 0xff v. 0x00 for unused space:
+// OEM memcards were NAND flash. One thing that may go against intuition is that
+// the default state of a bit is 1, not 0. Furthermore the physical cards were
+// only able to flip a bit from 1 to 0, not from 0 to 1, so in order to do the
+// latter, an entire "erase block" would have to be reset to 1 bits.
+
+// 528 bytes per page of an OEM memcard, including ECC bytes.
+const uint16_t bytes_per_page = 528;
+
+// Bytes of a 8 MB OEM memcard. 528 bytes/page, 16,384 pages = 8,388,608 bytes
+const uint32_t oem_memcard_bytes = bytes_per_page * 16384;
+
+// 28 character sequence found at the start of a memcard superblock to indicate
+// it is formatted.
+const std::string format_string = "Sony PS2 Memory Card Format ";
+
+// Number of pages per erase block on an OEM memcard.
+const uint8_t erase_block_pages = 16;
+
+// Address of an 8 byte checksum. Documentation is less than helpful at
+// explaining what this is or why it exists (claiming this area is unused).
+// Perhaps it is a CRC?
+const uint32_t checksum_address = 528;
 
 class Memcard
 {
+	private:
+		uint16_t FIFO_size; // Total number of reply bytes
+		uint16_t data_size; // Reply bytes of just the data being sent/received
+		uint32_t get_erase_block_start(uint32_t address);
+
+		bool formatted;
+		
+		std::string file_name;
+		std::array<char, oem_memcard_bytes> raw_data;
     public:
         Memcard();
+		
+		enum MODE {
+			INIT,
+			PAGE,
+			PAGE_ERASE,
+			PAGE_WRITE,
+			PAGE_READ,
+			GET_TERMINATOR,
+			AUTHENTICATION,
+			PS1
+		};
+
+		MODE mode = INIT;
+
+		uint8_t port;
+		uint8_t slot;
+		uint8_t command;
+		uint8_t terminator; // Container for terminator byte
+		uint32_t page_index; // Container for target page of a page op
+		uint32_t page_address; // Container for target address of a page op
+
+		bool good_page;
+		bool page_selected;
+		bool is_formatted();
+		bool address_in_bounds(uint32_t address, uint32_t length);
+		bool new_memcard_file(std::string file_name);
+		bool open(std::string file_name);
+		bool read(std::vector<uint8_t> destination, uint32_t address, uint32_t length);
+		bool write(std::vector<uint8_t> source, uint32_t address);
+		bool erase_block(uint32_t address);
+		bool is_erase_block_erased(uint32_t page_number);
+
+		void op_start(std::queue<uint8_t>& FIFO, uint8_t value);
+		void op_page(std::queue<uint8_t>& FIFO, uint8_t value);
+		void op_page_erase(std::queue<uint8_t>& FIFO, uint8_t value);
+		void op_page_write(std::queue<uint8_t>& FIFO, uint8_t value);
+		void op_page_read(std::queue<uint8_t>& FIFO, uint8_t value);
+		void op_get_terminator(std::queue<uint8_t>& FIFO, uint8_t value);
+		void op_authentication(std::queue<uint8_t>& FIFO, uint8_t value);
+		void op_ps1(std::queue<uint8_t>& FIFO, uint8_t value);
+		void push_terminators(std::queue<uint8_t>& FIFO);
 };
 
 #endif // MEMCARD_HPP
