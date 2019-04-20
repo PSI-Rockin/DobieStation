f90a9dcc3c536cec1130e9d61ccf7b39ad6798be
refractionpcsx2 <refraction@gmail.com>
2019-04-20 19:52:22 +0100

    Added PSI's bloody space :P

--- src/core/gscontext.cpp
+++ src/core/gscontext.cpp
@@ -38,7 +38,7 @@ void GSContext::set_tex0(uint64_t value)
     tex0.CLUT_base = ((value >> 37) & 0x3FFF) * 64 * 4;
     tex0.CLUT_format = (value >> 51) & 0xF;
     tex0.use_CSM2 = (value >> 55) & 0x1;
-    if(!tex0.CLUT_format)
+    if (!tex0.CLUT_format)
         tex0.CLUT_offset = ((value >> 56) & 0xF) * 16 * 2;
     else
         tex0.CLUT_offset = ((value >> 56) & 0x1F) * 16;
