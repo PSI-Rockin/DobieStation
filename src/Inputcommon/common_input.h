c32155bd824f3250edb09509fd7f4ebf4a842307
t o <finalfantasytimothy@gmail.com>
2020-01-05 22:27:53 -0700

    Visual Studio Changes

--- src/Inputcommon/common_input.h
+++ src/Inputcommon/common_input.h
@@ -2,7 +2,7 @@
 
 enum inputEvent{CROSS, TRIANGLE, CIRCLE, SQUARE, START, SELECT, R1, R2, R3, L1, L2, L3};
 
-class commonInput
+class common_input
 {
 	virtual void initalize();
 	virtual void sendInput();
