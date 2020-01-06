cfb4642873efd0e0f8892aa9756180e2c100a812
t o <finalfantasytimothy@gmail.com>
2020-01-05 21:39:57 -0700

    basic input structure

--- src/inputCommon/xinput.cpp
+++ src/inputCommon/xinput.cpp
@@ -2,17 +2,41 @@
 
 void xinput::initalize()
 {
-	XINPUT_STATE state;
 	for (DWORD i = 0; i < XUSER_MAX_COUNT; i++)
 	{
-		ZeroMemory(&isConnected, sizeof(state))
+		ZeroMemory(&isConnected, sizeof(state));
 
-			isConnected = XInputGetState(i, state);
+		isConnected = XInputGetState(i, &state);
 
 		if (isConnected == ERROR_SUCCESS) // Controller is connected
 		{
 			// handle basic controller shit in here my dudes
 		}
+	}
+}
 
+void xinput::sendInput()
+{
+	switch (state.Gamepad.wButtons)
+	{
+	case XINPUT_GAMEPAD_A:
+		event = inputEvent::CROSS;
+		break;
+	
+	case XINPUT_GAMEPAD_B:
+		event = inputEvent::CIRCLE;
+		break;
+
+	case XINPUT_GAMEPAD_X:
+		event = inputEvent::SQUARE;
+		break;
+
+	case XINPUT_GAMEPAD_Y:
+		event = inputEvent::TRIANGLE;
+		break;
+
+	case XINPUT_GAMEPAD_START:
+		event = inputEvent::START;
+		break;
 	}
 }
