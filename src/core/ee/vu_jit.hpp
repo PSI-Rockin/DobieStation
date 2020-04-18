f514b4ddaee80f6d5bbc91814868576a7778a83c
refractionpcsx2 <refraction@gmail.com>
2020-04-18 21:07:33 +0100

    further cleanup

--- src/core/ee/vu_jit.hpp
+++ src/core/ee/vu_jit.hpp
@@ -7,9 +7,9 @@ class VectorUnit;
 namespace VU_JIT
 {
 
-    uint16_t run(VectorUnit* vu);
-    void reset(VectorUnit *vu);
-    void set_current_program(uint32_t crc, VectorUnit *vu);
+uint16_t run(VectorUnit* vu);
+void reset(VectorUnit *vu);
+void set_current_program(uint32_t crc, VectorUnit *vu);
 
 };
 
