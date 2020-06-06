2ef0cbf914bb9f45f7ff58cbbf3741c55fe5ee45
kenshen112 <finalfantasytimothy@gmail.com>
2020-06-06 12:07:43 -0700

    Squashed several commits. Soo. Many. Merge Conflicts

--- src/qt/pad_configure.cpp
+++ src/qt/pad_configure.cpp
@@ -28,21 +28,6 @@ PadConfigure::PadConfigure(QWidget *parent) :
     
 #ifdef __linux__
 
-    button_widgets = new QButtonGroup(this);
-    button_widgets->addButton(ui->Select);
-    
-    
-    button_widgets->addButton(ui->Start);
-    button_widgets->addButton(ui->Dpad_Up);
-    button_widgets->addButton(ui->Dpad_Down);
-    button_widgets->addButton(ui->Dpad_Left);    
-    button_widgets->addButton(ui->Dpad_Right);
-    
-    button_widgets->addButton(ui->Triangle);
-    button_widgets->addButton(ui->Circle);
-    button_widgets->addButton(ui->Cross);
-    button_widgets->addButton(ui->Square);
-    
     EvdevDevices = pad.get_interesting_devices();
 
     for(int i = 0; i < pad.get_interesting_devices().size(); i++)
@@ -60,11 +45,8 @@ PadConfigure::PadConfigure(QWidget *parent) :
 
 #endif
 
-    uint current_button = (uint)pad.getEvent(selected_device);
 
-    std::cout << "KeyCode: " << unsigned(current_button) << std::endl;
-
-    button_map.emplace((BUTTONS)button, current_button);
+    selected_device = Interesting->currentIndex();
 
     connect(button_widgets, static_cast<void(QButtonGroup::*)(int)>(&QButtonGroup::buttonClicked), this, &PadConfigure::get_input);
     
