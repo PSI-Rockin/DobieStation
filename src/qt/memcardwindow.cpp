9e30d965290ed2f8d63bc87c48e070ae6e8d61e1
PSI-Rockin <PSI-Rockin@users.noreply.github.com>
2020-06-05 22:27:39 -0400

    Qt: Allow users to create unformatted cards

--- src/qt/memcardwindow.cpp
+++ src/qt/memcardwindow.cpp
@@ -2,6 +2,8 @@
 #include <QFileDialog>
 #include <QFileInfo>
 
+#include <fstream>
+
 #include "memcardwindow.hpp"
 #include "settings.hpp"
 #include "ui_memcardwindow.h"
@@ -21,6 +23,24 @@ MemcardWindow::MemcardWindow(QWidget *parent) :
             this, tr("Open Memcard"), Settings::instance().last_used_directory,
             tr("PS2 memcard image (*.ps2)")
         );
+
+        Settings::instance().set_memcard_path(path);
+        Settings::instance().save();
+    });
+
+    connect(ui->create_button, &QAbstractButton::clicked, this, [=]() {
+        QString path = QFileDialog::getSaveFileName(
+            this, tr("Create Memcard"), Settings::instance().last_used_directory,
+            tr("PS2 memcard image (*.ps2)")
+        );
+
+        //Page size of 0x200 (+ ECC), 0x4000 pages, for a total of 0x840000 bytes, the size of an 8 MB card
+        size_t memcard_size = (0x200 + 16) * 0x4000;
+
+        std::ofstream file(path.toStdString());
+        file.seekp(memcard_size - 1);
+        file.write("", 1);
+        file.close();
 
         Settings::instance().set_memcard_path(path);
         Settings::instance().save();
