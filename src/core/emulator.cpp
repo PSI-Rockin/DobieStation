9e05150a75ab8a7bdf5fc28e89c8a287a3ead5db
refractionpcsx2 <refraction@gmail.com>
2020-06-07 18:50:32 +0100

    Move render_CRT back to the end of VBLANK to resolve some threading issues

--- src/core/emulator.cpp
+++ src/core/emulator.cpp
@@ -231,7 +231,6 @@ void Emulator::vblank_start()
     VBLANK_sent = true;
     gs.set_VBLANK(true);
 
-    gs.render_CRT();
     timers.gate(true, true);
     cdvd.vsync();
     //cpu.set_disassembly(frames >= 223 && frames < 225);
@@ -248,6 +247,7 @@ void Emulator::vblank_end()
     timers.gate(true, false);
     frame_ended = true;
     frames++;
+    gs.render_CRT();
 }
 
 void Emulator::cdvd_event()
