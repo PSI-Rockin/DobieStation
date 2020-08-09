cb8772237e3c8333ded34d54bb429b89cc2dfec3
Mrlinkwii <Mrlinkwii@users.noreply.github.com>
2020-08-09 19:28:15 +0100

    Update emuwindow.cpp

--- src/qt/emuwindow.cpp
+++ src/qt/emuwindow.cpp
@@ -555,10 +555,10 @@ void EmuWindow::keyPressEvent(QKeyEvent *event)
             emit update_joystick(JOYSTICK::LEFT, JOYSTICK_AXIS::Y, 0xFF);
             break;
         case Qt::Key_G:
-            emit update_joystick(JOYSTICK::LEFT, JOYSTICK_AXIS::X, 0x00);
+            emit update_joystick(JOYSTICK::LEFT, JOYSTICK_AXIS::X, 0xFF);
             break;
         case Qt::Key_D:
-            emit update_joystick(JOYSTICK::LEFT, JOYSTICK_AXIS::X, 0xFF);
+            emit update_joystick(JOYSTICK::LEFT, JOYSTICK_AXIS::X, 0x00);
             break;
         case Qt::Key_I:
             emit update_joystick(JOYSTICK::RIGHT,JOYSTICK_AXIS::Y, 0x00); 
@@ -566,10 +566,10 @@ void EmuWindow::keyPressEvent(QKeyEvent *event)
         case Qt::Key_K:
             emit update_joystick(JOYSTICK::RIGHT,JOYSTICK_AXIS::Y, 0xFF);
         case Qt::Key_J:
-            emit update_joystick(JOYSTICK::RIGHT,JOYSTICK_AXIS::X, 0x00); 
+            emit update_joystick(JOYSTICK::RIGHT,JOYSTICK_AXIS::X, 0xFF); 
             break;
         case Qt::Key_L:
-             emit update_joystick(JOYSTICK::RIGHT,JOYSTICK_AXIS::X, 0xFF);
+             emit update_joystick(JOYSTICK::RIGHT,JOYSTICK_AXIS::X, 0x00);
              break;
         case Qt::Key_F1:
             if(!Settings::instance().recent_roms.isEmpty())
